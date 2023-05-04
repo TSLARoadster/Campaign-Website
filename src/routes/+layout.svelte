4ccb83a502b95bccd74a50e634cd9a3346ff842e
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2023-05-03 20:32:59 -0400

    Finish nav bar and landing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,81 @@
+<nav>
+    <div class="nav-container">
+        <a class="title" href="/"><b>VOTE FOR <span>STARS</span></b></a>
+        <div class="options">
+            <a class="lesser-button option" href="#mission">Our Mission</a>
+            <a class="lesser-button option" href="#about">About Us</a>
+            <a class="feedback-button option" href="#feedback">Send Feedback</a>
+        </div>
+    </div>
+</nav>
+
+<slot></slot>
+
+<style>
+    nav {
+        background-color: var(--blue-200);
+        position: sticky;
+        top: 0
+    }
+
+    .nav-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.5em 2em;
+    }
+
+    .title {
+        font-size: clamp(12pt, 7vw, 60pt);
+        width: 100%;
+        color: var(--blue-500)
+    }
+
+    .title span {
+        color: var(--red-200);
+        text-shadow: 0 0.07em var(--blue-500)
+    }
+
+    .options {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        list-style: none;
+        position: relative;
+    }
+
+    .option {
+        color: var(--gray-200);
+        font-family: "Montserrat Thin",sans-serif;
+        font-size: clamp(1pt, 2vw, 16pt);
+    }
+
+    .lesser-button {
+        transition: all 0.2s;
+        padding: 0.5em;
+        border-bottom: 0 solid;
+    }
+
+    .lesser-button:hover {
+        color: var(--blue-400);
+        border-bottom: 0.2em solid var(--blue-400);
+        transition: all 0.2s;
+    }
+
+
+    .feedback-button {
+        border: 0.2em solid var(--blue-500);
+        border-radius: 50px;
+        padding: 1em 1em;
+        display: flex;
+        align-items: center;
+        transition: background-color 0.3s, color 0.2s;
+    }
+
+    .feedback-button:hover {
+        background-color: var(--blue-500);
+        color: #ffffff;
+    }
+</style>
---
+++ src/routes/root/Landing.svelte
@@ -0,0 +1,76 @@
+<div class="landing">
+    <p><b>S</b>UPPORTING<br>
+        <b>T</b>EAMWORK<br>
+        <b>A</b>CHIEVEMENT AND<br>
+        <b>R</b>ESPONSIBILITY FOR<br>
+        <b>S</b>UCCESS
+    </p>
+    <div class="image-container">
+        <img src="static/images/Group Photo.png" alt="Group">
+        <div class="name-container">
+            <span class="name"><a href="/analee">Analee T</a></span>
+            <span class="name"><a href="/javion">Javion L</a></span>
+            <span class="name"><a href="/alan">Alan A</a></span>
+            <span class="name"><a href="/evie">Eviem'ree M</a></span>
+            <span class="name"><a href="/jimia">Jimia L</a></span>
+        </div>
+    </div>
+</div>
+
+<style>
+    .landing {
+        color: var(--red-200);
+        font-size: 5.5vw;
+        margin: 3vh 1vw;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-content: flex-start;
+        flex-wrap: wrap;
+    }
+
+    b {
+        font-weight: bolder;
+    }
+
+    .landing p {
+        margin: 2vw 2vw;
+        padding: 0;
+    }
+
+    img {
+        margin: 2vw 2vw;
+        width: 35vw;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+    }
+
+    @media only screen and (max-width: 800px) {
+        img {
+            width: 60vw;
+        }
+    }
+
+    .image-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .name-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-evenly;
+        width: 100%;
+    }
+
+    .name {
+        font-size: 2vw;
+        font-weight: bold;
+    }
+
+    .name a {
+        color: var(--blue-500)
+    }
+</style>
