902d0ed27bb584c0cd83081c5b510086de78a747
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2023-05-04 23:59:37 -0400

    Temporarily finished Jimia and Evie

---
+++ src/routes/evie/+page.svelte
@@ -0,0 +1,11 @@
+<img src="images/Temp Evie.png" alt="Evie" />
+
+<style>
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/routes/jimia/+page.svelte
@@ -0,0 +1,11 @@
+<img src="images/Temp Jimia.png" alt="Jimia" />
+
+<style>
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 100%;
+        height: 100%;
+    }
+</style>
